e6cc173f7b53097e43ae0239a2cd806d433c2a6b
AmitMizrahi123 <amitmizrahi231055@gmail.com>
2020-04-29 17:50:11 +0300

    add models form

---
+++ models/Contact.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const ContactSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("contact", ContactSchema);
--- routes/contact.js
+++ routes/contact.js
@@ -2,16 +2,53 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
+const Contact = require("../models/Contact");
+
 // @route   GET /api/contact
 // @desc    Get data
-router.get("/", (req, res) => {
-  res.send("Get the view");
+router.get("/", async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route   POST /api/contact
 // @desc    Send data
-router.post("/", (req, res) => {
-  res.send("Insert data and send");
-});
+router.post(
+  "/",
+  check("name", "Name is required").not().isEmpty(),
+  check("email", "Email is required").not().isEmpty(),
+  check("comment", "Comment is required").not().isEmpty(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, comment } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        comment,
+        phone,
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
