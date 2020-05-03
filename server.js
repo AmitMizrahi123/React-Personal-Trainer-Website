db2672c2691ca14d104015685e1b7cd55c7925e1
AmitMizrahi123 <amitmizrahi231055@gmail.com>
2020-05-03 22:20:15 +0300

    commit

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const favicon = require("express-favicon");
 const connectDB = require("./config/db");
 const path = require("path");
 const http = require("http");
@@ -11,23 +10,25 @@ const server = http.Server(app);
 connectDB();
 
 // Set static folder
-app.use(favicon(__dirname + "/client/build/favicon.ico"));
-app.use(express.static(path.join(__dirname, "client", "public")));
+app.use(express.static("client/public"));
 
-app.get("/ping", function (req, res) {
-  return res.send("pong");
+app.get("/api/home", function (req, res) {
+  res.json("success");
 });
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
+  console.log("pass");
   // Set static folder
   app.use(express.static("client/build"));
+
+  console.log("pass");
 
   app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => console.log(`server started in ${PORT}`));
