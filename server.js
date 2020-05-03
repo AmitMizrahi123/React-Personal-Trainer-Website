16535c7ef16dd7dd190412015d1853488172f83e
AmitMizrahi123 <amitmizrahi231055@gmail.com>
2020-05-03 10:38:57 +0300

    chnage server.js

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const app = express();
 
 // Connect Database
 connectDB();
 
+// CORS Middleware
+app.use(cors());
+// Logger Middleware
+app.use(morgan("dev"));
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
 // Set static folder
 app.use(express.static(path.join(__dirname, "client", "public")));
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "public", "index.html"));
 });
 
-// Serve static assets in production
+// Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static("client/build/static"));
+  app.use(express.static("client/build"));
 
-  app.get("/*", (req, res) =>
-    res.sendFile(path.join(__dirname, "client/build", "index.html"))
-  );
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
